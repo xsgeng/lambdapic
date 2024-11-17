042900614da65535e2e9a13d2d82acff1e2df282
gengxs <xsgeng@siom.ac.cn>
2024-11-17 15:19:48 +0800

    Refactor `create_store_density_cb` function into `ExtractSpeciesDensity` class for better readability and maintainability.

--- lambdapic/callback/utils.py
+++ lambdapic/callback/utils.py
@@ -49,42 +49,45 @@ def save_fields_to_hdf5(sim: Simulation, fields: list, every: int, prefix: [Path
     return save_fields
 
 
-def create_store_density_cb(sim: Simulation, species: Species, every: int) -> tuple[np.ndarray, callable]:
-    ispec_target = sim.species.index(species)
-    buf = np.zeros((sim.nx, sim.ny))
+class ExtractSpeciesDensity:
+    stage = "current deposition"
+    def __init__(self, sim: Simulation, species: Species, every: int):
+        self.sim = sim
+        self.species = species
+        self.every = every
+        self.ispec_target = sim.species.index(species)
+        self.density = np.zeros((sim.nx, sim.ny))
+        
+        self.patches = sim.patches
+        self.nx_per_patch = sim.nx_per_patch
+        self.ny_per_patch = sim.ny_per_patch
+        self.n_guard = sim.n_guard
 
-    patches = sim.patches
-    nx_per_patch = sim.nx_per_patch
-    ny_per_patch = sim.ny_per_patch
-    n_guard = sim.n_guard
+    def _get_patch_slice(self, patch):
+        return np.s_[
+            patch.ipatch_x*self.nx_per_patch:(patch.ipatch_x+1)*self.nx_per_patch,
+            patch.ipatch_y*self.ny_per_patch:(patch.ipatch_y+1)*self.ny_per_patch
+        ]
 
-    if ispec_target == 0:
-        def store_density(it, ispec):
-            if it % every == 0:
+    def __call__(self, it, ispec):
+        if it % self.every == 0:
+            if self.ispec_target == 0:
                 if ispec == 0:
-                    for ipatch, p in enumerate(patches):
-                        s = np.s_[p.ipatch_x*nx_per_patch:p.ipatch_x*nx_per_patch+nx_per_patch,\
-                                p.ipatch_y*ny_per_patch:p.ipatch_y*ny_per_patch+ny_per_patch]
-                        buf[s] = p.fields.rho[:-2*n_guard, :-2*n_guard] / sim.species[ispec].q
-
-    elif ispec_target > 0:
-        def store_density(it, ispec):
-            if it % every == 0:
-                if ispec == ispec_target - 1:
-                    for ipatch, p in enumerate(patches):
-                        s = np.s_[p.ipatch_x*nx_per_patch:p.ipatch_x*nx_per_patch+nx_per_patch,\
-                                p.ipatch_y*ny_per_patch:p.ipatch_y*ny_per_patch+ny_per_patch]
+                    for p in self.patches:
+                        s = self._get_patch_slice(p)
+                        self.density[s] = p.fields.rho[:-2*self.n_guard, :-2*self.n_guard] / self.sim.species[ispec].q
+            else:
+                if ispec == self.ispec_target - 1:
+                    for p in self.patches:
+                        s = self._get_patch_slice(p)
                         # store previous rho
-                        buf[s] = p.fields.rho[:-2*n_guard, :-2*n_guard]
-                if ispec == ispec_target:
-                    for ipatch, p in enumerate(patches):
-                        s = np.s_[p.ipatch_x*nx_per_patch:p.ipatch_x*nx_per_patch+nx_per_patch,\
-                                p.ipatch_y*ny_per_patch:p.ipatch_y*ny_per_patch+ny_per_patch]
+                        self.density[s] = p.fields.rho[:-2*self.n_guard, :-2*self.n_guard]
+                if ispec == self.ispec_target:
+                    for p in self.patches:
+                        s = self._get_patch_slice(p)
                         # subtract previous rho
-                        buf[s] = p.fields.rho[:-2*n_guard, :-2*n_guard] - buf[s]
-                        buf[s] /= sim.species[ispec].q
-
-    return buf, store_density
+                        self.density[s] = p.fields.rho[:-2*self.n_guard, :-2*self.n_guard] - self.density[s]
+                        self.density[s] /= self.sim.species[ispec].q
 
 
 def species_transfer(s1, s2):
