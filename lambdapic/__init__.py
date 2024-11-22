8d6548eeaaa70bf2644839734f4808a1a2a83a37
gengxs <xsgeng@siom.ac.cn>
2024-11-22 14:12:12 +0800

    remove species.py. use libpic.species

--- lambdapic/__init__.py
+++ lambdapic/__init__.py
@@ -1,3 +1,3 @@
-from .species import Species, Electron, Proton
+from libpic.species import Species, Electron, Proton, Photon
 from .simulation import Simulation
 from .callback.callback import callback
