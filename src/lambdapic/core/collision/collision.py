93bd7c868a927c59d7cad2e28b59b615d54c0cb0
gengxs <xsgeng@siom.ac.cn>
2025-09-12 17:01:55 +0800

    feat: implement collision class interface

--- src/lambdapic/core/collision/collision.py
+++ src/lambdapic/core/collision/collision.py
@@ -1,29 +1,54 @@
 
-from typing import Any, Sequence
+from typing import Any, List, Sequence, Tuple
 
+import numpy as np
+from numba import typed
 from scipy.constants import c, epsilon_0, pi
 
 from ..patch.patch import Patches
-from ..sort.particle_sort import ParticleSort2D
+from ..sort.particle_sort import ParticleSort2D, ParticleSort3D
 from ..species import Species
+from .cpu import debye_length_patches
+from .utils import ParticleData, pack_particle_data
 
 
 class Collission:
-    def __init__(
-        self,
-        patches: Patches,
-        species: Sequence[Species],
-        sorter: Sequence[ParticleSort2D]
-    ):
+    def __init__(self, collision_species: Sequence[Tuple[Species, Species]], patches: Patches, sorter: Sequence[ParticleSort2D]):
+        self.collision_species = collision_species
+
+        self.all_species = patches.species
         self.patches = patches
-        self.ispec1 = patches.species.index(species[0])
-        self.ispec2 = patches.species.index(species[1])
         self.sorter = sorter
 
-        self.q1 = species[0].q
-        self.m1 = species[0].m
-        self.q2 = species[1].q
-        self.m2 = species[1].m
+        self.cell_vol = self.patches.dx
+        if self.patches.dimension >= 2:
+            self.cell_vol *= self.patches.dy
+        if self.patches.dimension == 3:
+            self.cell_vol *= self.patches.dz
+
+    def generate_particle_lists(self) -> None:
+        self.part_lists: List[List[ParticleData]] = []
+        for s in self.all_species:
+            self.part_lists.append(
+                typed.List(# type: ignore
+                    pack_particle_data([p.particles[s.ispec] for p in self.patches], s.m, s.q)
+                )
+            )
+
+    def generate_field_lists(self) -> None:
+        self.debye_length_inv_sqare_list = typed.List(# type: ignore
+            [np.zeros((p.nx, p.ny), dtype=np.float64) for p in self.patches]
+        )
+
+    def calculate_debye_length(self) -> None:
+        for s in self.all_species:
+            debye_length_patches(
+                self.part_lists[s.ispec],
+                self.sorter[s.ispec].bucket_bound_min_list,
+                self.sorter[s.ispec].bucket_bound_max_list,
+                self.cell_vol,
+                self.debye_length_inv_sqare_list
+            )
 
     def __call__(self) -> Any:
         pass
